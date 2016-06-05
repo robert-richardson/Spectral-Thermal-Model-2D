6093e1cb52d07e5b16ba843507ba1cba2253a08e
Robert Richardson <robert-richardson@users.noreply.github.com>
2016-06-05 18:42:21 +0100

    Update MainScript.m

--- MainScript.m
+++ MainScript.m
@@ -2,19 +2,6 @@
 %               2-D Spectral-Galerkin Battery Thermal Model               %
 %-------------------------------------------------------------------------%
 
-%{
-My name is Robert Richardson (robert.richardson@eng.ox.ac.uk) and I hold 
-the MIT license for this code. An accompanying paper for this code entitled
-"On-board monitoring of 2-D spatially-resolved temperatures
-in cylindrical lithium-ion batteries: Part I. Low-order thermal modelling"
-has been submitted for publication in the Journal of Power Sources. A 
-preprint for this paper can be found at: http://arxiv.org
-
-I would ask that you cite this paper if you want to use this code for 
-your own research. For further details on the work of the Energy Power 
-Group at Oxford, please see epg.eng.ox.ac.uk.
-%}
-
 % This code implements a Chebyshev Spectral Galerkin 2D thermal model and
 % compares it against a finite element (FEM) model using Matlab PDEtool.
 % Execution of the Mainscript.m file runs the simulation.
@@ -25,6 +12,14 @@ Group at Oxford, please see epg.eng.ox.ac.uk.
 % 'fem':    stores Finite-Element model inputs/outputs
 % 'mult':   higher level struct storing both model outputs at multiple time instances
 
+% An accompanying paper for this code entitled
+% "On-board monitoring of 2-D spatially-resolved temperatures
+% in cylindrical lithium-ion batteries: Part I. Low-order thermal modelling"
+% has been submitted for publication in the Journal of Power Sources.
+
+% Copyright (c) 2016, The Chancellor, Masters and Scholars of the University 
+% of Oxford, and the 'Spectral li-ion SPM' Developers.
+% See the licence file LICENCE.txt for more information.
 
 %% Initialise
 clear; 
